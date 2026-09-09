e9cc6bbe9ff360cbd80968ba01bdbd02108545d9
Michael Kage <michaelkage9@gmail.com>
2026-09-09 14:18:32 +0100

    feat: add fluid route transitions

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,28 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import TopNav from './TopNav'
 import BottomNav from './BottomNav'
 import NotificationBanner from '@/components/ui/NotificationBanner'
 
 export default function Layout() {
+  const location = useLocation()
   return (
     <div className="min-h-screen bg-obsidian flex flex-col">
       <TopNav />
       <NotificationBanner />
       <main className="flex-1 pb-20 md:pb-0">
-        <Outlet />
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 8 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -5 }}
+            transition={{ duration: 0.18, ease: 'easeOut' }}
+            className="min-h-full"
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
       <BottomNav />
     </div>
